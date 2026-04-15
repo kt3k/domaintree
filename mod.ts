d7d64e695f90bbddc4164fd2c72a6328670071ed
Yoshiya Hinosawa <stibium121@gmail.com>
2026-04-15 11:04:55 +0900

    chore: remove temporary plan files, and clean up

--- mod.ts
+++
@@ -1,9 +0,0 @@
-export { parse } from "./src/parser.ts";
-export { render } from "./src/renderer.ts";
-export type {
-  DisplayGroup,
-  DomainDocument,
-  DomainObject,
-  DomainObjectNode,
-  Property,
-} from "./src/types.ts";
